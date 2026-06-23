d927417de9bfbed960c1349b734cb24f00326c59
we-expand <cleber.couto@we-expand.com>
2026-06-23 08:08:43 -0300

    fix: injetada chave direta do Resend na rota de API

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
 // Inicializa o Resend com a variável de ambiente segura
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(re_NHnfHYcv_ADPQcaTLGxwiyU31Xv8fbYVZ);
 
 export async function POST(request: Request) {
   try {
